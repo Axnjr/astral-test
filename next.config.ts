37cb1a9fb78fb8bafa0fcf8d7f752fdb7c7df945
Govind <yakshitchhipa@gmail.com>
2025-07-18 05:29:22 +0530

    chore

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com', 'images.pexels.com', 'fastly.picsum.photos'],
+    domains: ['images.unsplash.com', 'images.pexels.com', 'fastly.picsum.photos', 'picsum.photos', 'examples.motion.dev', 'framerusercontent.com'],
   },
 };
 
