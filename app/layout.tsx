187f26ccffbce5210d741745a44f4d98d6444395
Govind <yakshitchhipa@gmail.com>
2025-07-18 09:51:08 +0530

    feat: wrap children in EventsProvider and AnimatePresence for improved event context and animations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AnimatePresence } from "framer-motion";
+import { EventsProvider } from "@/contexts/EventContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <EventsProvider>
+          <AnimatePresence mode="popLayout" initial={false}>
+            {children}
+          </AnimatePresence>
+        </EventsProvider>
       </body>
     </html>
   );
